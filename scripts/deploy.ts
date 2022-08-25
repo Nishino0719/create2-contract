1f93159a60a29ecf2aafc97f350227358521f5ae
Nishino0719 <nishino0719@icloud.com>
2022-08-25 12:24:45 +0900

    🚧  create deploy script

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,18 +1,14 @@
 import { ethers } from "hardhat";
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+  const Greeter = await ethers.getContractFactory("Greeter");
+  const greeter = await Greeter.deploy("Hello, Hardhat!");
 
-  const lockedAmount = ethers.utils.parseEther("1");
-
-  const Lock = await ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
-
-  console.log(`Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`);
+  await greeter.deployed();
+  console.log(
+    `Greeter Deploy Tx: https://ropsten.etherscan.io/tx/${greeter.deployTransaction.hash}`
+  );
+  console.log(`Greeter Contract Address: ${greeter.address}`);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
